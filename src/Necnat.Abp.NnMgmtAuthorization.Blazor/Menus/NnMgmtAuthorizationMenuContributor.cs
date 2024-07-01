9e033811db7fbcff05c094bbc04e73480b884bb3
Evandro Mota de Souza <evandromosou@outlook.com>
2024-07-01 00:08:42 -0300

    Att

--- src/Necnat.Abp.NnMgmtAuthorization.Blazor/Menus/NnMgmtAuthorizationMenuContributor.cs
+++ src/Necnat.Abp.NnMgmtAuthorization.Blazor/Menus/NnMgmtAuthorizationMenuContributor.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using Necnat.Abp.NnMgmtAuthorization.Localization;
+using Necnat.Abp.NnMgmtAuthorization.Permissions;
+using System.Threading.Tasks;
+using Volo.Abp.Identity;
 using Volo.Abp.UI.Navigation;
 
 namespace Necnat.Abp.NnMgmtAuthorization.Blazor.Menus;
@@ -13,11 +16,83 @@ public class NnMgmtAuthorizationMenuContributor : IMenuContributor
         }
     }
 
-    private Task ConfigureMainMenuAsync(MenuConfigurationContext context)
+    private async Task ConfigureMainMenuAsync(MenuConfigurationContext context)
     {
-        //Add main menu items.
-        context.Menu.AddItem(new ApplicationMenuItem(NnMgmtAuthorizationMenus.Prefix, displayName: "NnMgmtAuthorization", "/NnMgmtAuthorization", icon: "fa fa-globe"));
+        var l = context.GetLocalizer<NnMgmtAuthorizationResource>();
 
-        return Task.CompletedTask;
+        bool displayauthorizationMenu = false;
+        var authorizationMenu = new ApplicationMenuItem(
+            NnMgmtAuthorizationMenus.Prefix,
+            l["Menu:NnMgmtAuthorization"],
+            icon: "fas fa-user-shield"
+        );
+
+        bool displayAuthorizationConfiguracaoMenu = false;
+        var authorizationConfiguracaoMenu = new ApplicationMenuItem(
+            NnMgmtAuthorizationMenus.Configuration,
+            l["Menu:NnMgmtAuthorization:Configuration"],
+            order: 1
+        );
+
+        if (await context.IsGrantedAsync(NnMgmtAuthorizationPermissions.PrmAuthEndpoint.Default))
+        {
+            authorizationConfiguracaoMenu.AddItem(new ApplicationMenuItem(
+                NnMgmtAuthorizationMenus.Configuration_AuthEndpoint,
+                l["Menu:NnMgmtAuthorization:Configuration:AuthEndpoint"],
+                url: "/NnMgmtAuthorization/Configuration/AuthEndpoints",
+                order: 1
+            ));
+            displayAuthorizationConfiguracaoMenu = true;
+        }
+
+        if (await context.IsGrantedAsync(NnMgmtAuthorizationPermissions.PrmHierarchy.Default))
+        {
+            authorizationConfiguracaoMenu.AddItem(new ApplicationMenuItem(
+                NnMgmtAuthorizationMenus.Configuration_Hierarchy,
+                l["Menu:NnMgmtAuthorization:Configuration:Hierarchy"],
+                url: "/NnMgmtAuthorization/Configuration/Hierarchies",
+                order: 2
+            ));
+            displayAuthorizationConfiguracaoMenu = true;
+        }
+
+        if (await context.IsGrantedAsync(NnMgmtAuthorizationPermissions.PrmHierarchyComponentGroup.Default))
+        {
+            authorizationConfiguracaoMenu.AddItem(new ApplicationMenuItem(
+                NnMgmtAuthorizationMenus.Configuration_HierarchyComponentGroup,
+                l["Menu:NnMgmtAuthorization:Configuration:HierarchyComponentGroup"],
+                url: "/NnMgmtAuthorization/Configuration/HierarchyComponentGroups",
+                order: 3
+            ));
+            displayAuthorizationConfiguracaoMenu = true;
+        }
+
+        if (await context.IsGrantedAsync(NnMgmtAuthorizationPermissions.PrmHierarchicalStructure.Default))
+        {
+            authorizationConfiguracaoMenu.AddItem(new ApplicationMenuItem(
+                NnMgmtAuthorizationMenus.Configuration_HierarchicalStructure,
+                l["Menu:NnMgmtAuthorization:Configuration:HierarchicalStructure"],
+                url: "/NnMgmtAuthorization/Configuration/HierarchicalStructure",
+                order: 4
+            ));
+            displayAuthorizationConfiguracaoMenu = true;
+        }
+
+        if (displayAuthorizationConfiguracaoMenu)
+            authorizationMenu.AddItem(authorizationConfiguracaoMenu);
+
+        if (await context.IsGrantedAsync(IdentityPermissions.Users.Default))
+        {
+            authorizationMenu.AddItem(new ApplicationMenuItem(
+                NnMgmtAuthorizationMenus.User,
+                l["Menu:NnMgmtAuthorization:User"],
+                url: "/NnMgmtAuthorization/Users",
+                order: 2
+            ));
+            displayauthorizationMenu = true;
+        }
+
+        if (displayauthorizationMenu || displayAuthorizationConfiguracaoMenu)
+            context.Menu.AddItem(authorizationMenu);
     }
 }
